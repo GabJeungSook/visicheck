07c018708b4aead93e54163c109061fe2443aa81
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-04-16 16:24:08 +0800

    asdasd

--- lib/widgets/news_item.dart
+++ lib/widgets/news_item.dart
@@ -27,7 +27,7 @@ class NewsItem extends StatelessWidget {
                 color: primary,
                 ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 14,
           ),
           Expanded(
@@ -35,46 +35,46 @@ class NewsItem extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Name: ' + data.name,
+                  '${data.name}',
                   overflow: TextOverflow.ellipsis,
                   maxLines: 1,
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 4,
                 ),
                 Text(
-                  'Purpose: ' + data.purpose,
+                  'Purpose: ${data.purpose}',
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                   style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 4,
                 ),
                  Text(
-                  'From: ' + data.department,
+                  'Visitor Type: ${data.department}',
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                   style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
                 ),
-                 SizedBox(
+                 const SizedBox(
                   height: 4,
                 ),
                  Text(
-                  'Visited At: ' + data.department_type,
+                  'Destination: ${data.department_type}',
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                   style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
                 ),
-                 SizedBox(
+                 const SizedBox(
                   height: 4,
                 ),
                 Text(
                  data.timestamp != null
                   ? DateFormat('MMMM dd, yyyy hh:mm a').format(DateTime.parse(data.timestamp))
                   : '',
-                  style: TextStyle(
+                  style: const TextStyle(
                       fontWeight: FontWeight.w300,
                       fontStyle: FontStyle.italic,
                       fontSize: 12),
